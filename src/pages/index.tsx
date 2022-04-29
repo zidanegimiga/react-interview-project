1e8f556962e864c49dce5db8dfcab5ba9a3c723e
zidanegimiga <zidanegimiga@gmail.com>
2022-04-30 00:39:36 +0300

    Display Users Table in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
+import UsersTable from '../components/UserTable'
 
 const Home: NextPage = () => {
   return (
-    <>Hello World</>
+    <>
+      <UsersTable />
+    </>
   )
 }
 
