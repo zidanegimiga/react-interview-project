709c1347da08d5dc492ae14c02274b2a67950e27
zidanegimiga <zidanegimiga@gmail.com>
2022-05-04 12:21:28 +0300

    fix: resolved incorrect naming of the Homepage styles module
    For the project to build successfully on vercel, the modification of the
    styles import module has been made.
    
    Closes #03

--- src/components/HomePageCard.tsx
+++ src/components/HomePageCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from "../../styles/HomepageCard.module.scss";
+import styles from "../../styles/HomePageCard.module.scss";
 import { titleStyle } from './constants/inlineStyle';
 
 import Button from './commons/Button';
