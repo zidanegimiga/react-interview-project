61955d5d88f80a70a8876f932ac3ae7e9a40ef18
zidanegimiga <zidanegimiga@gmail.com>
2022-05-04 13:02:08 +0300

    feat: implement progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 /* eslint-disable */
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; 
 import '../../styles/globals.scss';
 
 import reportWebVitals from '../reportWebVitals';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
