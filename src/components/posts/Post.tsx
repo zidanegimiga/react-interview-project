280cc708fdae108044948ad21eb17405d1650a94
zidanegimiga <zidanegimiga@gmail.com>
2022-05-12 13:19:16 +0300

    feat: enable user to click username to open user page

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,4 +1,5 @@
 /*eslint-disable*/
+import Link from "next/link";
 import styles from "../../../styles/Posts.module.scss";
 
 const Post = (props) => {
@@ -6,7 +7,9 @@ const Post = (props) => {
   return (
     post?.map((post) =>(
         <div className={styles.PostCard} key={post.postId}>
-            <span className={styles.postName}>{post.userName}</span>
+            <Link href={`/user/${post.userId}`} passHref>
+              <span className={styles.postName}>{post.userName}</span>
+            </Link>
             <span className={styles.postTitle}>{post.title}</span>
             <span className={styles.postBody}>{post.body}</span>
         </div>
