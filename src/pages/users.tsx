09351896534112ff3b2be8f6c509c4d9d0060177
zidanegimiga <zidanegimiga@gmail.com>
2022-05-12 11:25:11 +0300

    refactor: switch data fetching method
    - data fetching method switch from useSWR to getStaticProps

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,26 +1,25 @@
 /*eslint-disable */
-import useSWR from "swr";
-import { baseUrl, getUsers } from "../../lib/users";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import axios from "axios";
+import { baseUrl} from "../../lib/users";
 import { getSuiteCount } from "../components/utils/utils";
 import Navbar from "../components/global/Navbar";
 import UserCard from "../components/Users/UserCard";
 import StatsCard from "../components/Users/StatsCard";
 import styles from "../../styles/Users.module.scss";
 
-const Users = () => {
-  const { data, isValidating } = useSWR(baseUrl.concat("/users"), getUsers);
-  const response = getSuiteCount(data);
+const Users = ({users}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const response = getSuiteCount(users);
 
   return (
     <>
       <Navbar />
       <div className={styles.wrapper}>
         <h1>User</h1>
-        {isValidating ? <p> Loading</p> : null}
 
         <div className={styles.main}>
           <div className={styles.userCardsContainer}>
-            {data?.map((user) => (
+            {users?.map((user) => (
               <UserCard {...user} />
             ))}
           </div>
@@ -34,5 +33,16 @@ const Users = () => {
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const {data: users} = await axios.get(baseUrl.concat("/users"));
+
+  return {
+    props: {
+      users
+    },
+    revalidate: 5
+  }
+}
 
 export default Users;
